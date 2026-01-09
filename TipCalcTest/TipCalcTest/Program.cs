354ae0238d644969249432effdd09beb0140151f
jrg6526 <dtc6526@gmail.com>
2026-01-09 13:40:10 -0700

    add C# testing folder

---
+++ TipCalcTest/TipCalcTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalcTest
+{
+    public class Price
+    {
+        public static double GetTip(double price, double qos)
+        {
+            return price * (qos / 100);
+        }
+        public static double GetTotalPrice(double price, double tip)
+        {
+            return price + tip;
+        }
+        static void Main()
+        {
+        }
+    }
+}
---
+++ TipCalcTest/UnitTestProject1/CalcTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TipCalcTest;
+
+namespace TipUnitTest
+{
+    [TestClass]
+    public class PriceTest
+    {
+        [DataRow(10.00, 15, 1.50)]
+        [DataRow(15.00, 18, 2.70)]
+        [DataRow(25.00, 22, 5.50)]
+
+        [DataTestMethod]
+        public void TestTip(double price, double qos, double tip)
+        {
+            Assert.AreEqual(Price.GetTip(price, qos), tip, 0.01);
+        }
+
+        [DataRow(20.00, 1.50, 21.50)]
+        [DataRow(10.50, 3.00, 13.50)]
+        [DataRow(2.00, 1.00, 3.00)]
+
+        [DataTestMethod]
+        public void TestTotal(double price, double tip, double total)
+        {
+            Assert.AreEqual(Price.GetTotalPrice(price, tip), total, 0.01);
+        }
+    }
+}
